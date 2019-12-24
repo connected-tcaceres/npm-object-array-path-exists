6b5872e00a797832c204367705c4c38b23988179
tylerccaceres <tyler.caceres@gmail.com>
2019-12-24 13:26:22 -0500

    include null case

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,7 @@ const pathExists = (object, objectPath) => {
   for (const varPath of path) {
     concatenatedPath += `['${varPath}']`;
     let data = eval(`object${concatenatedPath}`);
-    if (data === undefined) {
+    if (data === undefined || data === null) {
       return false;
     }
   }
